3c140f17c68df529bf02d9b7e3213ba1f69146f3
Juan Antonio de los Santos <juansantoszarco@gmail.com>
2015-02-02 12:05:22 +0100

    Initial Commit

---
+++ Everpobre/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Everpobre
+//
+//  Created by Juan Antonio de los Santos on 02/02/15.
+//  Copyright (c) 2015 Juan Antonio de los Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Everpobre/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Everpobre
+//
+//  Created by Juan Antonio de los Santos on 02/02/15.
+//  Copyright (c) 2015 Juan Antonio de los Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
